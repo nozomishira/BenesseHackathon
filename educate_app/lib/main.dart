770276a9eb3f0d6e35b47b1af6873cce0f3f9bdc
ShiraishiNozomi <baishiwang1213@gmail.com>
2020-11-14 15:56:17 +0900

    文字の色を変えた

--- educate_app/lib/main.dart
+++ educate_app/lib/main.dart
@@ -124,6 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
                         ),
                         onPressed: () {
